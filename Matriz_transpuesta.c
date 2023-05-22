f7ce621f842a568787904c56363a9e715a75e8b0
Mati <amaticc03@gmail.com>
2023-05-22 16:01:14 -0500

    Primer commit

--- Matriz_transpuesta.c
+++ Matriz_transpuesta.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int tamano;
+
+    printf("Ingrese el tamano de la matriz: ");
+    scanf("%d", &tamaño);
+
+    int matriz[tamaño][tamaño];
+
+    //Generar numeros aleatorios entre 0 y 100
+
+    srand(time(NULL));
+
+    //LLenar la mtriz con numeros aleatorios
+     for (int i = 0; i < tamaño; i++) {
+        for (int j = 0; j < tamaño; j++) {
+            matriz[i][j] = rand() % 101; // Generar número aleatorio entre 0 y 100
+        }
+    }
+
+    
+    
+
+
+
+
+}
