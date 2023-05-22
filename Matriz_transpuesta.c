70acac4a096f1ac95b6cab77ef75d87db3cff83b
Mati <amaticc03@gmail.com>
2023-05-22 16:13:21 -0500

    Commit final

--- Matriz_transpuesta.c
+++ Matriz_transpuesta.c
@@ -6,9 +6,9 @@ int main() {
     int tamano;
 
     printf("Ingrese el tamano de la matriz: ");
-    scanf("%d", &tamaño);
+    scanf("%d", &tamano);
 
-    int matriz[tamaño][tamaño];
+    int matriz[tamano][tamano];
 
     //Generar numeros aleatorios entre 0 y 100
 
@@ -23,20 +23,31 @@ int main() {
 
     //Imprimir la matriz original
      printf("Matriz original:\n");
-    for (int i = 0; i < tamaño; i++) {
-        for (int j = 0; j < tamaño; j++) {
+    for (int i = 0; i < tamano; i++) {
+        for (int j = 0; j < tamano; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
 
-    //imrprimir la matriz transpuesta 
+    //calcular la matriz transpuesta 
+int transpuesta[tamano][tamano];
+    for (int i = 0; i < tamano; i++) {
+        for (int j = 0; j < tamano; j++) {
+            transpuesta[i][j] = matriz[j][i];
+         }
+     }
 
+     // imprimir matriz transpuesta 
 
-    
-    
+      printf("Matriz transpuesta:\n");
+    for (int i = 0; i < tamano; i++) {
+        for (int j = 0; j < tamano; j++) {
+            printf("%d ", transpuesta[i][j]);
+        }
+        printf("\n");
+    }
 
-
-
-
+return 0;
 }
+// en el commit final se arreglo el nombre de las variables por que escribi tamaño en ves de tamano como estaba en la variable, se agrego el calculo de la mtriz transpuesta y el calculo de la matriz transpuesta 
